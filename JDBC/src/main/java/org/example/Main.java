9e0bca2bb5b0bf943b8fffb8fcb6cc718ee54148
Adityamishra-18 <madityamishra05@gmail.com>
2026-07-21 13:37:24 +0530

    updated

--- JDBC/src/main/java/org/example/Main.java
+++ JDBC/src/main/java/org/example/Main.java
@@ -1,43 +1,104 @@
 package org.example;
 
+import com.mysql.cj.protocol.Resultset;
+
 import java.sql.*;
 import java.util.Scanner;
+//
+////TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+//// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//public class Main {
+//    public static void main(String[] args) {
+//        String URL ="jdbc:mysql://127.0.0.1:3306/student";
+//        String USER = "root";
+//        String PASSWORD = "aditya@2005";
+//        try {
+//
+//          Connection con = DriverManager.getConnection(URL,USER,PASSWORD);
+//
+//          PreparedStatement pr=con.prepareStatement("insert into student_data values (?,?,?)");
+//            System.out.println("Enter id  : \n name :\n Domain :");
+//            Scanner sc = new Scanner(System.in);
+//
+//
+//          pr.setInt(1,sc.nextInt());
+//          sc.nextLine();
+//          pr.setString(2,sc.nextLine());
+//          pr.setString(3,sc.nextLine());
+//
+//          //4th Step(Execute Query)
+//           int rowsaffect = pr.executeUpdate();
+//            System.out.println(rowsaffect);
+//            if(rowsaffect>0){
+//                System.out.println("Data inserted");
+//            }
+//            con.close();
+//
+//
+//
+//        } catch ( SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//
+//    }
+//}
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        String URL = "jdbc:mysql://127.0.0.1:3306/student";
+//        String USER = "root";
+//        String PASSWORD = "aditya@2005";
+//
+//        try {
+//            Connection con = DriverManager.getConnection(URL, USER, PASSWORD);
+//
+//            Statement st = con.prepareStatement();
+//
+//            ResultSet rs = st.executeQuery("SELECT * FROM student_data");
+//
+//            while (rs.next()) {
+//                System.out.println("ID : " + rs.getInt("id"));
+//                System.out.println("Name : " + rs.getString("name"));
+//                System.out.println("Domain : " + rs.getString("domain"));
+//            }
+//
+//            rs.close();
+//            st.close();
+//            con.close();
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
+
+
+
 public class Main {
     public static void main(String[] args) {
-        String URL ="jdbc:mysql://127.0.0.1:3306/student";
+
+        String URL = "jdbc:mysql://127.0.0.1:3306/student";
         String USER = "root";
         String PASSWORD = "aditya@2005";
+
         try {
+            Connection con = DriverManager.getConnection(URL, USER, PASSWORD);
 
-          Connection con = DriverManager.getConnection(URL,USER,PASSWORD);
-
-          PreparedStatement pr=con.prepareStatement("insert into student_data values (?,?,?)");
-            System.out.println("Enter id  : \n name :\n Domain :");
-            Scanner sc = new Scanner(System.in);
-
-
-          pr.setInt(1,sc.nextInt());
-          sc.nextLine();
-          pr.setString(2,sc.nextLine());
-          pr.setString(3,sc.nextLine());
-
-          //4th Step(Execute Query)
-           int rowsaffect = pr.executeUpdate();
-            System.out.println(rowsaffect);
-            if(rowsaffect>0){
-                System.out.println("Data inserted");
+            PreparedStatement prt = con.prepareStatement("SELECT * FROM student_data where id =?");
+            prt.setInt(1    ,2);
+            ResultSet rst=prt.executeQuery();
+            while(rst.next()){
+                System.out.println("ID : " + rst.getInt("id"));
+                System.out.println("Name : " + rst.getString("name"));
+                System.out.println("Domain : " + rst.getString("domain"));
             }
-            con.close();
 
 
-
-        } catch ( SQLException e) {
-            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-
-
     }
 }
