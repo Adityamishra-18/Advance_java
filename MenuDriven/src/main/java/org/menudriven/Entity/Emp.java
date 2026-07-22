061b63e5f17682150a4e4173f1eeebe5d5215224
Adityamishra-18 <madityamishra05@gmail.com>
2026-07-22 23:01:31 +0530

    MenuDriven project

--- MenuDriven/src/main/java/org/menudriven/Entity/Emp.java
+++ MenuDriven/src/main/java/org/menudriven/Entity/Emp.java
@@ -16,35 +16,27 @@ public class Emp {
     public Emp() {
 
     }
-
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getDepartment() {
         return department;
     }
-
     public void setDepartment(String department) {
         this.department = department;
     }
-
     public double getSalary() {
         return salary;
     }
-
     public void setSalary(double salary) {
         this.salary = salary;
     }
