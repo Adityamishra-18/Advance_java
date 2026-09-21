1539ec509266ad88b8ba1bb679fda05aae2b0fd0
Adityamishra-18 <madityamishra05@gmail.com>
2026-09-21 20:48:18 +0530

    add hibernate menudriven project

--- Hibernate/src/main/java/Driver.java
+++ Hibernate/src/main/java/Driver.java
@@ -4,22 +4,75 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
+import java.util.Scanner;
+
 public class Driver {
+    private static EntityManagerFactory emf;
+    private static EntityTransaction et;
+    private static EntityManager em;
+    private static Scanner sc;
+    static{
+        emf=Persistence.createEntityManagerFactory("HND");
+        em=emf.createEntityManager();
+        et=em.getTransaction();
+        sc=new Scanner(System.in);
+    }
     public static void main(String[] args) {
-        EntityManagerFactory emf=Persistence.createEntityManagerFactory("HND");
-        EntityManager em=emf.createEntityManager();
-        EntityTransaction et=em.getTransaction();
+        int choice=0;
 
-        //Student s1=new Student(101,"Aditya","Java Fullstack");
-        et.begin();
-//        Student student=em.find(Student.class,102);
-//        System.out.println(student);
+        do{
+            System.out.println("1.Add Student\n2.Update the Student\n3.Find by id\n4.Delete Student");
+            choice=sc.nextInt();
+            switch (choice){
+                case 1:
+                    Student st=new Student();
+                    System.out.println("Enter the Student id:");
+                    st.setId(sc.nextInt());
+                    System.out.println("enter Student name");
+                    sc.nextLine();
+                    st.setName(sc.nextLine());
+                    System.out.println("Enter Student Course");
+                    st.setCourse(sc.nextLine());
+                    add(st);
+                break;
 
-//        em.remove(101);
+                case 2:
+                    Student st1=new Student();
+                    System.out.println("Enter id to update");
+                    int id=sc.nextInt();
+                    update(id);
+                break;
+            }
+
+        }while(choice!=0);
+    }
 
 
-//        em.persist(s1);
-//        em.merge(s1);
-        et.commit();
+    private static boolean add(Student st) {
+
+        if(st!=null){
+            et.begin();
+            em.persist(st);
+            et.commit();
+            return true;
+        }
+
+        return false;
+    }
+    private static String update(int id){
+        Student std=em.find(Student.class,id);
+        if(std!=null){
+            System.out.println("Enter the new course");
+            sc.nextLine();
+            std.setCourse(sc.nextLine());
+            System.out.println("Enter the new name");
+            std.setName(sc.nextLine());
+            et.begin();
+            em.merge(std);
+            et.commit();
+            return "Success";
+        }else{
+            return"Student not found";
+        }
     }
 }
